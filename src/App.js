ee8b290b36f135a8437374a4c1180af8a9e6f24e
APEREZB26 <aperez26@autonoma.edu.pe>
2022-02-23 20:37:42 -0500

    Uso de Axios y Control de Promesas

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
 import "./App.css";
 import { useEffect, useState } from "react";
-// import { Notes } from "./components/Notes";
 import axios from "axios";
 import { Country } from "./components/Country";
+import {getAllCountry} from "./services/country/getAllCountry";
+import {addCountry} from "./services/country/addCountry";
 
 const App = () => {
   const [country, setCountry] = useState([]);
+  const [newCountry, setNewcountry] = useState(0);
 
   useEffect(() => {
-    axios.get("https://fakestoreapi.com/users").then((res) => {
-      const { data } = res;
-      setCountry(data);
-    });
+    getAllCountry().then((country)=>{
+        setCountry(country)
+    })
   }, []);
+
+  const st ={
+      color : "black",
+  }
+
+  const handleChange = (event)  =>{
+    setNewcountry(event.target.value)
+  }
+
+  const handleSubmit = (e) =>{
+    e.preventDefault();
+
+    const countryAddToState = {
+      title: newCountry,
+      body: newCountry,
+      userId: 1
+    }
+
+    addCountry(countryAddToState).then((newNote)=>{
+      setCountry((prevCountry)=>prevCountry.concat(newNote))
+    })
+    setNewcountry("")
+  }
+
 
   return (
     <div>
       {country.map((info)=>{
-        return <Country key={info.id} id={info.id} username={info.username} />
+        return <Country key={info.id} id={info.id} title={info.title} body={info.body} />
       })}
+      <p>{newCountry}</p>
+      <input style={st} onChange={handleChange}></input>
+      <button style={st} onClick={handleSubmit}>Hola</button>
     </div>
   );
 };
--- src/components/Country.js
+++ src/components/Country.js
@@ -1,9 +1,14 @@
-export const Country = ({id,email,username}) =>{
+export const Country = ({id,title,body}) =>{
+    const style ={
+        border: "2px solid aliceblue",
+        margin: "10px",
+        padding : "10px",
+    }
     return(
-        <div>
-            <b>{id}</b>
-            <p>{email}</p>
-            <p>{username}</p>
+        <div style={style}>
+            <h1>{title}</h1>
+            <p>{body}</p>
+            <b>ID: {id}</b>
         </div>
     )
 }
